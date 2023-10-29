a62a9f5ee47e39d5366e4c4bc99ee099b1082977
ritendra28 <ritendra.28@gmail.com>
2023-10-29 10:14:47 +0530

    typo fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.header('Breakfas Menu')
 
-streamlit.task('Omega 3, Blue Barry Meals')
-streamlit.task('Kale, Spinach and Rocket Smoothie')
-streamlit.task('Hard Boiled Free Range Egg')
+streamlit.text('Omega 3, Blue Barry Meals')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Hard Boiled Free Range Egg')
