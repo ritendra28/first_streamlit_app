4beee2c871545ad0cce2df9a569dc8d9bacdc190
ritendra28 <ritendra.28@gmail.com>
2023-10-29 10:11:40 +0530

    added code changes streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Breakfas Menu')
+
+streamlit.task('Omega 3, Blue Barry Meals')
+streamlit.task('Kale, Spinach and Rocket Smoothie')
+streamlit.task('Hard Boiled Free Range Egg')
